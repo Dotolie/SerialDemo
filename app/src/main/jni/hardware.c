1e9eb8f1e9ce2e278ad6f0d915ad8c0a13897c0a
Dotolie <cygdrive@naver.com>
2020-04-09 00:04:03 +0900

    add hardware.h & serial.h in jni.

--- app/src/main/jni/hardware.c
+++ app/src/main/jni/hardware.c
@@ -19,48 +19,7 @@
 #define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
 
 static const char *TAG="Hardware";
-static const char *pDevice[2] = {
-    "/dev/ttyS0",
-    "/dev/tty",
-};
 
-static speed_t getBaudrate(jint baudrate)
-{
-	switch(baudrate) {
-	case 0: return B0;
-	case 50: return B50;
-	case 75: return B75;
-	case 110: return B110;
-	case 134: return B134;
-	case 150: return B150;
-	case 200: return B200;
-	case 300: return B300;
-	case 600: return B600;
-	case 1200: return B1200;
-	case 1800: return B1800;
-	case 2400: return B2400;
-	case 4800: return B4800;
-	case 9600: return B9600;
-	case 19200: return B19200;
-	case 38400: return B38400;
-	case 57600: return B57600;
-	case 115200: return B115200;
-	case 230400: return B230400;
-	case 460800: return B460800;
-	case 500000: return B500000;
-	case 576000: return B576000;
-	case 921600: return B921600;
-	case 1000000: return B1000000;
-	case 1152000: return B1152000;
-	case 1500000: return B1500000;
-	case 2000000: return B2000000;
-	case 2500000: return B2500000;
-	case 3000000: return B3000000;
-	case 3500000: return B3500000;
-	case 4000000: return B4000000;
-	default: return -1;
-	}
-}
 
 static const char *pGpios[7] = {
     "/sys/class/gpio/gpio35/value",
@@ -136,7 +95,7 @@ JNIEXPORT jint JNICALL Java_com_friendlyarm_FriendlyThings_HardwareController_ge
  * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_friendlyarm_FriendlyThings_HardwareController_setTxMode
-  (JNIEnv *, jclass, jint)
+  (JNIEnv *env, jclass thiz, jint pin)
  {
 
     return 0;
@@ -148,7 +107,7 @@ JNIEXPORT jint JNICALL Java_com_friendlyarm_FriendlyThings_HardwareController_se
  * Signature: (I)I
  */
 JNIEXPORT jint JNICALL Java_com_friendlyarm_FriendlyThings_HardwareController_setRxMode
-  (JNIEnv *, jclass, jint)
+  (JNIEnv *env, jclass thiz, jint pin)
 {
     return 0;
 }
